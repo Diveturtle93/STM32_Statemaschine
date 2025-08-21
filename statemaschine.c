feddeba87a641dc111bd08b0456b944177c09acc
Diveturtle93 <Hafemeister1@hotmail.de>
2025-08-21 04:38:24 +0200

    Hinzufügen der Daten
    - Typedefines
    - Funktionen für State und Statemaschine
    - Status setzen

---
+++ statemaschine.c
@@ -0,0 +1,195 @@
+//----------------------------------------------------------------------
+// Titel	:	statemaschine.c
+//----------------------------------------------------------------------
+// Sprache	:	C
+// Datum	:	11.08.2025
+// Version	:	1.0
+// Autor	:	Diveturtle93
+// Projekt	:	Statemaschine
+// Quelle	:	
+//----------------------------------------------------------------------
+
+// Einfuegen der standard Include-Dateien
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+
+// Einfuegen der STM Include-Dateien
+//----------------------------------------------------------------------
+#include "main.h"
+//----------------------------------------------------------------------
+
+// Einfuegen der eigenen Include Dateien
+//----------------------------------------------------------------------
+#include "statemaschine.h"
+#include "BasicUart.h"
+//----------------------------------------------------------------------
+
+// Variablen einbinden
+//----------------------------------------------------------------------
+Statemaschine BMSstate = {{Start, true, false, false, false}};
+static uint32_t timeError = 0, timeWarning = 0;
+uint32_t longError = 0, longWarning = 0;
+//----------------------------------------------------------------------
+
+// Setze neuen State der Statemaschine
+//----------------------------------------------------------------------
+void setState(uint8_t State)
+{
+	// State auswaehlen
+	switch (State)
+	{
+		case Ready:
+		{
+			// State setzen
+			BMSstate.State = Ready;
+			uartTransmit("Ready\n", 6);
+
+			break;
+		}
+		case KL15:
+		{
+			// State setzen
+			BMSstate.State = KL15;
+			uartTransmit("KL15\n", 5);
+
+			break;
+		}
+		case Anlassen:
+		{
+			// State setzen
+			BMSstate.State = Anlassen;
+			uartTransmit("Anlassen\n", 9);
+
+			break;
+		}
+		case Precharge:
+		{
+			// State setzen
+			BMSstate.State = Precharge;
+			uartTransmit("Precharge\n", 10);
+
+			break;
+		}
+		case ReadyToDrive:
+		{
+			// State setzen
+			BMSstate.State = ReadyToDrive;
+			uartTransmit("ReadyToDrive\n", 13);
+
+			break;
+		}
+		case Drive:
+		{
+			// State setzen
+			BMSstate.State = Drive;
+			uartTransmit("Drive\n", 6);
+
+			break;
+		}
+		case Laden:
+		{
+			// State setzen
+			BMSstate.State = Laden;
+			uartTransmit("Laden\n", 6);
+
+			break;
+		}
+		case Standby:
+		{
+			// State setzen
+			BMSstate.State = Standby;
+			uartTransmit("Standby\n", 8);
+
+			break;
+		}
+		case Ausschalten:
+		{
+			// State setzen
+			BMSstate.State = Ausschalten;
+			uartTransmit("Ausschalten\n", 12);
+
+			break;
+		}
+		default:
+		{
+			break;
+		}
+	}
+}
+//----------------------------------------------------------------------
+
+// Setze Fehler Status der Statemaschine
+//----------------------------------------------------------------------
+void setStatus(uint8_t Status)
+{
+	// Fehler Status setzen
+	switch (Status & 0xF0)
+	{
+		case StateNormal:
+		{
+			// Wenn Fehler Status Warnung
+			if (BMSstate.Status & StateWarning)
+			{
+				// Wenn letzte Warnung schon laenger als 30s nicht mehr aufgetreten
+				if (millis() > (timeWarning + WARNING_RESET))
+				{
+					// Status zuruecksetzen
+					BMSstate.Status = (Status | BMSstate.State);
+
+					// Fehlervariablen zuruecksetzen
+					longWarning = 0;
+					longError = 0;
+				}
+
+				break;
+			}
+		}
+		case StateWarning:
+		{
+			// Zeit abspeichern
+			timeWarning = millis();
+
+			// Wenn Fehler Status Error
+			if (BMSstate.Status & StateError)
+			{
+				// Wenn letzter Error schon laenger als 5min nicht mehr aufgetreten
+				if (millis() > (timeError + ERROR_RESET))
+				{
+					// Status zuruecksetzen
+					BMSstate.Status = (StateWarning | BMSstate.State);
+				}
+
+				break;
+			}
+		}
+		case StateError:
+		{
+			// Zeit abspeichern
+			timeError = millis();
+
+			// Wenn Fehler Status kritischer Fehler
+			if (BMSstate.Status & CriticalError)
+			{
+				// Status setzen ueberspringen
+				break;
+			}
+		}
+		case CriticalError:
+		{
+			// Status setzen
+			BMSstate.Status = (Status | BMSstate.State);
+			break;
+		}
+		default:
+		{
+			// Status setzen
+			BMSstate.Status = (CriticalError | BMSstate.State);
+
+			// Fehlerausgabe wenn Status nicht korrekt
+			uartTransmit("BMS Kritischer Fehler Statemaschine\n!", 36);
+			break;
+		}
+	}
+}
+//----------------------------------------------------------------------
---
+++ statemaschine.h
@@ -0,0 +1,75 @@
+//----------------------------------------------------------------------
+// Titel	:	statemaschine.h
+//----------------------------------------------------------------------
+// Sprache	:	C
+// Datum	:	11.08.2025
+// Version	:	1.0
+// Autor	:	Diveturtle93
+// Projekt	:	Statemaschine
+//----------------------------------------------------------------------
+
+// Dateiheader definieren
+//----------------------------------------------------------------------
+#ifndef INC_STATEMASCHINE_H_
+#define INC_STATEMASCHINE_H_
+//----------------------------------------------------------------------
+
+// Konstanten definieren
+//----------------------------------------------------------------------
+#define BMSTIME										300000					// Standby Zeit nach ausschalten KL15, 5min
+#define ERROR_RESET									300000					// Zeit 5min bis Error Zurueckgesetzt werden kann
+#define WARNING_RESET								30000					// Zeit 30s bis Warning Zurueckgesetzt werden kann
+//----------------------------------------------------------------------
+
+// Definiere Statemaschine Typedefines
+//----------------------------------------------------------------------
+typedef enum
+{
+	Start,																	// 0 Starte Batteriemanagement
+	Ready,																	// 1 Batteriemanagement gestartet
+	KL15,																	// 2 KL15 aktiv
+	Anlassen,																// 3 Anlasser betaetigt
+	Precharge,																// 4 Precharge Fahrzeug
+	ReadyToDrive,															// 5 Batteriemanagement bereit fuer Fahrmodus
+	Drive,																	// 6 Fahrzeug im Fahrmodus
+	Standby,																// 7 Auto wird abgeschaltet, Zeitverzoegerung bis Batteriemanagement ausgeht
+	Ausschalten,															// 8 Batteriemanagement ausschalten
+	Laden,																	// 9 Lademodus
+	StateNormal = 0x10,														// 16 Normalzustand
+	StateWarning = 0x20,													// 32 Warnung
+	StateError = 0x40,														// 64 Fehler
+	CriticalError = 0x80,													// 128 Critischer Fehler
+} states;
+//----------------------------------------------------------------------
+typedef union
+{
+	struct
+	{
+		uint8_t State : 4;													// State der Statemaschine
+		uint8_t Normal : 1;													// Statemaschine Normal
+		uint8_t Warning : 1;												// Statemaschine Warning
+		uint8_t Error : 1;													// Statemaschine Error
+		uint8_t CriticalError : 1;											// Statemaschine kritischer Error
+	};
+
+	uint8_t Status;									// 1 Byte
+} Statemaschine;
+//----------------------------------------------------------------------
+
+// Definiere globale Variablen
+//----------------------------------------------------------------------
+extern Statemaschine BMSstate;												// Variable fuer BMS-Statemaschine definieren
+extern uint32_t longError;													// Variable fuer Langzeit-Fehler, um Fehler eine bestimmte Zeit noch anzuzeigen, auch wenn nicht mehr vorhanden
+extern uint32_t longWarning;												// Variable fuer Langzeit-Warnung, um Warnung eine bestimmte Zeit noch anzuzeigen, auch wenn nicht mehr vorhanden
+//----------------------------------------------------------------------
+
+// Funktionen definieren
+//----------------------------------------------------------------------
+void setState (uint8_t state);												// State setzen
+void setStatus (uint8_t status);											// Status setzen
+uint32_t getLongError (void);												// Langzeit-Fehler zurueckgeben
+uint32_t getLongWarning (void);												// Langzeit-Warning zurueckgeben
+//----------------------------------------------------------------------
+
+#endif /* INC_STATEMASCHINE_H_ */
+//----------------------------------------------------------------------
